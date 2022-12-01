d671d48ae3efe4c1d2b3a16a9c1b19be274fbab7
Deathkebab <111434502+Deathkebaba@users.noreply.github.com>
2022-12-01 20:49:53 +0100

    Found bug with starring champions

--- UnitTests/PlayerTests.py
+++ UnitTests/PlayerTests.py
@@ -2,11 +2,18 @@ from Simulator.player import player
 from Simulator.pool import pool
 from Simulator.champion import champion
 
-
-def add_to_bench_empty_bench(p1):
-    testChampion = champion("evelynn")
-    assert p1.add_to_bench(testChampion)
-
+#Creates Zilean 11 times, there are 3 1* Zileans and a 3* Zilean, correlated to update_triple_catalogue ...
+def add_to_bench_empty_bench(p1: player):
+    #create a champ
+    p1.gold = 100000
+    p1.max_units = 1000
+    print(p1.bench)
+    for x in range(11):
+        loopChampion = champion("zilean")
+        assert p1.buy_champion(loopChampion)
+    print(p1.bench)
+    for x in range(4):
+        print(p1.bench[x].stars)
 
 def setup():
     base_pool = pool()
