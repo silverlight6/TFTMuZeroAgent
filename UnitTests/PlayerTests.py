76b2c8338a9b2c854ece48d188f87b01656f49dc
Silver Lucoris <slucoris@gmail.com>
2022-12-04 15:39:10 -0800

    fixing main branch stuff, be back to this after

--- UnitTests/PlayerTests.py
+++ UnitTests/PlayerTests.py
@@ -2,11 +2,13 @@ from Simulator.player import player
 from Simulator.pool import pool
 from Simulator.champion import champion
 
+
 def setup() -> player:
     """Creates fresh player and pool"""
     base_pool = pool()
     player1 = player(base_pool, 0)
     return player1
+
 
 def level2Champion():
     """Creates 3 Zileans, there should be 1 2* Zilean on bench"""
@@ -18,11 +20,12 @@ def level2Champion():
         p1.buy_champion(champion("zilean"))
     print("Bench: ", p1.bench)
     assert p1.bench[0].stars == 2, "champion should be 2*"
-    for x in range(1,9):
-        assert p1.bench[x] == None, "this slot should be empty"
+    for x in range(1, 9):
+        assert p1.bench[x] is None, "this slot should be empty"
     for x in p1.board:
         for y in x:
             assert y == None, "the board should be empty"
+
 
 def level3Champion():
     """Creates 9 Zileans, there should be 1 3* Zilean on bench"""
@@ -34,11 +37,12 @@ def level3Champion():
         p1.buy_champion(champion("zilean"))
     print("Bench: ", p1.bench)
     assert p1.bench[0].stars == 3, "champion should be 3*"
-    for x in range(1,9):
+    for x in range(1, 9):
         assert p1.bench[x] == None, "this slot should be empty"
     for x in p1.board:
         for y in x:
-            assert y == None, "the board should be empty"    
+            assert y == None, "the board should be empty"
+
 
 def levelChampFromField():
     """buy third copy while 1 copy on field"""
@@ -47,14 +51,15 @@ def levelChampFromField():
     p1.max_units = 1000
     p1.buy_champion(champion("zilean"))
     p1.buy_champion(champion("zilean"))
-    p1.move_bench_to_board(1,0,0)
+    p1.move_bench_to_board(1, 0, 0)
     p1.buy_champion(champion("zilean"))
-    #print("Bench: ", p1.bench)
-    #print("Field: ", p1.board)
-    #print(p1.bench[0].stars)
+    # print("Bench: ", p1.bench)
+    # print("Field: ", p1.board)
+    # print(p1.bench    [0].stars)
     for x in p1.bench:
-        assert x == None, "bench should be empty"
+        assert x is None, "bench should be empty"
     assert p1.bench[0][0].stars == 2, "the unit placed on the field should be 2*"
+
 
 def buyExp():
     p1 = setup()
@@ -63,9 +68,10 @@ def buyExp():
     while p1.level < p1.max_level:
         p1.exp = p1.level_costs[p1.level + 1]
         p1.level_up()
-        lvl +=1
+        lvl += 1
         assert lvl == p1.level
-    
+
+
 def spamExp():
     """buys tons of experience"""
     p1 = setup()
@@ -75,6 +81,7 @@ def spamExp():
     assert p1.level == p1.max_level, "I should be max level"
     assert p1.exp == 0, "I should not have been able to buy experience after hitting max lvl"
 
+
 def incomeTest1():
     """first test for gold income"""
     p1 = setup()
@@ -82,12 +89,14 @@ def incomeTest1():
     p1.gold_income(5)
     assert p1.gold == 21, f"Interest calculation is messy, gold should be 21, it is {p1.gold}"
 
+
 def incomeTest2():
     """Check for income cap"""
     p1 = setup()
     p1.gold = 1000
     p1.gold_income(5)
     assert p1.gold == 1010, f"Interest calculation is messy, gold should be 1010, it is {p1.gold}"
+
 
 def incomeTest3():
     """Checks win streak gold"""
@@ -121,6 +130,7 @@ def incomeTest3():
     p1.gold_income(5)
     assert p1.gold == 8, f"Interest calculation is messy, gold should be 8, it is {p1.gold}"
 
+
 def incomeTest4():
     """Checks loss streak gold"""
     p1 = setup()
@@ -153,22 +163,22 @@ def incomeTest4():
     p1.gold_income(5)
     assert p1.gold == 8, f"Interest calculation is messy, gold should be 8, it is {p1.gold}"
 
+
 def list_of_tests():
     """tests all test cases"""
-    #level2Champion()
-    #level3Champion()
-    #levelChampFromField()
-    
-    buyExp()
-    #spamExp()
-    
-    #Problem: Interest gets calculated after base income is added
-    #incomeTest1()
-    #Problem: Interest rate not capped
-    #incomeTest2()
-    #incomeTest3()
-    #incomeTest4()
-    
-    #I would like to go over move commands again before writing test code for that
+    level2Champion()
+    level3Champion()
+    levelChampFromField()
 
+    buyExp()
+    spamExp()
+
+    # Problem: Interest gets calculated after base income is added
+    incomeTest1()
+    # Problem: Interest rate not capped
+    incomeTest2()
+    incomeTest3()
+    incomeTest4()
+
+    # I would like to go over move commands again before writing test code for that
     pass
