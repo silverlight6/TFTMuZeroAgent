757c804cb821400c2adba488f19cf39a4ce66ae1
Jober't Aladwan <Jobert.Aladwan@gmail.com>
2022-12-14 15:22:45 -0600

    Added framework of minion combat method, restructured minion classes

--- Simulator/minion.py
+++ Simulator/minion.py
@@ -1,6 +1,6 @@
+import config
 from Simulator.item_stats import item_builds as full_items, basic_items, starting_items
 from Simulator import champion, pool, pool_stats
-from Simulator.stats import AD, HEALTH, ARMOR, MR, AS, RANGE, MANA, MAXMANA
 import random
 
 item_list = list(full_items.keys())
@@ -9,41 +9,41 @@ item_list = list(full_items.keys())
 # add randomness to drops
 # add representations of minions (Melees, Ranges, Krugs, Wolves, Raptors, Nexus, and Herald)
 # add combat between player and minions
-class Minion:
-    def __init__(self, name, y=-1, x=-1):
-        self.name = name
-        self.x = x
-        self.y = y
 
-        self.AD = AD[name]
-        self.AS = AS[name]
-        self.RANGE = RANGE[name]
-        self.ARMOR = ARMOR[name]
-        self.MR = MR[name]
-        self.HEALTH = HEALTH[name]
-        self.MANA = MANA[name]
-        self.MAXMANA = MAXMANA[name]
-class Melee(Minion):
-    def __init__(self, x, y):
-        super().__init__(self, 'meleeminion', x, y)
-class Ranged(Minion):
-    def __init__(self, x, y):
-        super().__init__(self, 'rangedminion', x, y)
+# Objects representing board configs for each minion round
+# These should function similar to player objects except simplified for minion combat
+class Minion:
+    def __init__(self):
+        # array of champions, since order does not matter, can be unordered list
+        self.bench = [None for _ in range(9)]
+        # Champion array, this is a 7 by 4 array.
+        self.board = [[None for _ in range(4)] for _ in range(7)]
+        # List of items, there is no object for this so this is a string array
+class FirstMinion(Minion):
+    def __init__(self):
+        super().__init__(self)
+        self.board[1][1] = champion('meleeminion', 1, 1, 1, None, None, None, False)
+class SecondMinion(Minion):
+    def __init__(self):
+        super().__init__(self)
+class ThirdMinion(Minion):
+    def __init__(self):
+        super().__init__(self)
 class Krug(Minion):
-    def __init__(self, x, y):
-        super().__init__(self, 'krug', x, y)
+    def __init__(self):
+        super().__init__(self)
 class Wolf(Minion):
-    def __init__(self, name, x, y):
-        super().__init__(self, name, x, y)
+    def __init__(self):
+        super().__init__(self)
 class Raptor(Minion):
-    def __init__(self, name, x, y):
-        super().__init__(self, name, x, y)
+    def __init__(self):
+        super().__init__(self)
 class Nexus(Minion):
-    def __init__(self, x, y):
-        super().__init__(self, 'nexus', x, y)
+    def __init__(self):
+        super().__init__(self)
 class Herald(Minion):
-    def __init__(self, x, y):
-        super().__init__(self, 'herald', x, y)
+    def __init__(self):
+        super().__init__(self)
 
 def minion_round(player, round, pool_obj):
  # simulate minion round here
@@ -96,6 +96,35 @@ def minion_round(player, round, pool_obj):
     else:
         return
 
-def minion_combat(player, round):
-    pass
+# modeled after combat_phase from game_round.py, except with a minion "player" versus the player
+def minion_combat(player, enemy, round):
+    ROUND_DAMAGE = [
+            [3, 0],
+            [9, 2],
+            [15, 3],
+            [21, 5],
+            [27, 8],
+            [10000, 15]
+        ]
+    config.WARLORD_WINS['red'] = player.win_streak
+    player.end_turn_actions()
+    player.start_round()
+
+    round_index = 0
+    while round > ROUND_DAMAGE[round_index][0]:
+            round_index += 1
+
+    index_won, damage = champion.run(champion.champion, player, enemy, ROUND_DAMAGE[round_index][1])
+    # tie!
+    if index_won == 0:
+        player.loss_round(round)
+        player.health -= damage
+    # player wins!
+    if index_won == 1:
+        player.won_round(round)
+    # minions win! (yikes)
+    if index_won == 2:
+        player.loss_round(round)
+        player.health -= damage
+    
 
