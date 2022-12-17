18022909e2ba4e0d8a4dd9c795a1541b66a30e04
Jober't Aladwan <Jobert.Aladwan@gmail.com>
2022-12-17 16:44:59 -0600

    added test to check if minions combat is functioning correctly

--- UnitTests/MinionTests.py
+++ UnitTests/MinionTests.py
@@ -9,6 +9,21 @@ def setup() -> player:
     player1 = player(base_pool, 0)
     return player1
 
+def combatTest():
+    p1 = setup()
+    p1.gold = 10000
+    p1.max_units = 100
+    # making 3* Zilean for combat
+    minion.minion_round(p1, 0, p1.pool_obj)
+    minion.minion_round(p1, 1, p1.pool_obj)
+    minion.minion_round(p1, 2, p1.pool_obj)
+    minion.minion_round(p1, 8, p1.pool_obj)
+    minion.minion_round(p1, 14, p1.pool_obj)
+    minion.minion_round(p1, 20, p1.pool_obj)
+    minion.minion_round(p1, 26, p1.pool_obj)
+    minion.minion_round(p1, 33, p1.pool_obj)
+
+    assert p1.health == 100, "I lost a PVE round despite having a 3* Zilean!"
 def round0Test():
     """Testing minion method usage at round 0"""
     p1 = setup()
@@ -17,9 +32,12 @@ def round0Test():
     # making 3* Zilean for combat
     for x in range(9):
         p1.buy_champion(champion("zilean"))
-    
+
     minion.minion_round(p1, 0, p1.pool_obj)
     assert not emptyBench(p1.item_bench), "I didn't get any items from the PVE round!"
+
+def round1Test():
+    pass
 
 def emptyBench(item_bench):
     for i in item_bench:
