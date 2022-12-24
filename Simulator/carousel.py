0e7d0c4889e8dcc67019f1d4fbe8119cbee867a2
Jober't Aladwan <Jobert.Aladwan@gmail.com>
2022-12-24 01:00:55 -0600

    added pool_obj to carousel method args, implemented generateAllComponents

--- Simulator/carousel.py
+++ Simulator/carousel.py
@@ -3,7 +3,7 @@ from Simulator.item_stats import item_builds as full_items, basic_items, startin
 from Simulator import champion, pool, pool_stats
 import random
 
-def carousel(players, round):
+def carousel(players, round, pool_obj):
     # probability of certain arrangements during certain carousels
     # https://leagueoflegends.fandom.com/wiki/Carousel_(Teamfight_Tactics)
     alive = []
@@ -14,8 +14,8 @@ def carousel(players, round):
             if player.health <= alive[0].health and player != alive[0]:
                 alive.insert(0, player)
 
-    champions = generateChampions(round)
-    items = generateHeldItems(round)
+    champions = generateChampions(round, pool_obj)
+    items = generateHeldItems(round, pool_obj)
 
     # give all champions on the carousel an item
     for champ in champions:
@@ -32,19 +32,17 @@ def carousel(players, round):
     pass
 
 # this will handle champion generation based on the current round
-def generateChampions(round):
+def generateChampions(round, pool_obj):
     pass
 
 # handles the item generation based on the current round
 # also chooses what kind of item set to generate (e.g. offensive components only, defensive, utility, etc.)
-def generateHeldItems(round):
+def generateHeldItems(round, pool_obj):
     pass
 
 def generateAllComponents():
     # generate a list of 1 of every component + 1 random duplicate component
+    items = []
     for i in range(0,9):
-        player.add_to_item_bench(starting_items[random.randint(0, len(starting_items) - 1)])
-        ran_cost_3 = list(pool_stats.COST_3.items())[random.randint(0, len(pool_stats.COST_3) - 1)][0]
-        ran_cost_3 = champion.champion(ran_cost_3)
-        pool_obj.update(ran_cost_3, -1)
-        player.add_to_bench(ran_cost_3)
+        items.append(starting_items[i])
+    return items
