713493a2eb04c51845909038b171c6926fb42840
Jober't Aladwan <Jobert.Aladwan@gmail.com>
2022-12-23 21:57:51 -0600

    started carousel.py file

---
+++ Simulator/carousel.py
@@ -0,0 +1,11 @@
+import config
+from Simulator.item_stats import item_builds as full_items, basic_items, starting_items
+from Simulator import champion, pool, pool_stats
+import random
+
+def carousel(players, round):
+    # probability of certain arrangements during certain carousels
+    # https://leagueoflegends.fandom.com/wiki/Carousel_(Teamfight_Tactics)
+
+    # initialize list of units w/ items for carousel
+    pass
