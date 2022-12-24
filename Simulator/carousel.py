cf3a5e584926ac1471c32b768239f078e6c96074
Jober't Aladwan <Jobert.Aladwan@gmail.com>
2022-12-24 00:06:45 -0600

    created basic framework of carousel round simulation

--- Simulator/carousel.py
+++ Simulator/carousel.py
@@ -6,6 +6,39 @@ import random
 def carousel(players, round):
     # probability of certain arrangements during certain carousels
     # https://leagueoflegends.fandom.com/wiki/Carousel_(Teamfight_Tactics)
+    alive = []
+    alive.append(players[0])
+    # sort the list of alive players with the lowest HP player at the beginning
+    for player in players:
+        if player:
+            if player.health <= alive[0].health and player != alive[0]:
+                alive.insert(0, player)
 
-    # initialize list of units w/ items for carousel
+    champions = generateChampions(round)
+    items = generateHeldItems(round)
+
+    # give all champions on the carousel an item
+    for champ in champions:
+        for i in items:
+            if items[i]:
+                champ.add_item(items[i])
+                items[i] = None
+                break
+    
+    for player in alive:
+        for champ in champions:
+            player.add_to_bench(champ)
+            player.generate_bench_vector()
     pass
+def generateChampions(round):
+    pass
+def generateHeldItems(round):
+    pass
+def generateAllComponents():
+    # generate a list of 1 of every component + 1 random duplicate component
+    for i in range(0,9):
+        player.add_to_item_bench(starting_items[random.randint(0, len(starting_items) - 1)])
+        ran_cost_3 = list(pool_stats.COST_3.items())[random.randint(0, len(pool_stats.COST_3) - 1)][0]
+        ran_cost_3 = champion.champion(ran_cost_3)
+        pool_obj.update(ran_cost_3, -1)
+        player.add_to_bench(ran_cost_3)
