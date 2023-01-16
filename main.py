4c9578774a94b874dedafe80b23d6ace988c0d9e
lobotuerk <tomas.lobo.it@gmail.com>
2023-01-16 18:27:11 -0300

    changed starting episode default to 0

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ def main():
     #TODO(lobotuerk) A lot of hardcoded parameters should be used like this intead.
     parser = argparse.ArgumentParser(description='Train an AI to play TFT',
                                 epilog='For more information, go to https://github.com/silverlight6/TFTMuZeroAgent')
-    parser.add_argument('--starting_episode', '-se', dest='starting_episode', type=int, 
+    parser.add_argument('--starting_episode', '-se', dest='starting_episode', type=int, default=0,
                         help='Episode number to start the training. Used for loading checkpoints, disables loading if = 0')
     args = parser.parse_args()
 
