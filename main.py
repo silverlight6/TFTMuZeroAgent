54d432bae0f8f53d572e7c9a9bd641c81f2d772a
oliverjdlopez <97719688+oliverjdlopez@users.noreply.github.com>
2023-01-21 04:28:06 -0500

    changed default run to test_train_model

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import config
 from UnitTests.BaseUnitTest import runTest
 import argparse
 
+
 def main():
     if config.RUN_UNIT_TEST:
         runTest()
 
-    #TODO(lobotuerk) A lot of hardcoded parameters should be used like this intead.
+    # TODO(lobotuerk) A lot of hardcoded parameters should be used like this intead.
     parser = argparse.ArgumentParser(description='Train an AI to play TFT',
-                                epilog='For more information, go to https://github.com/silverlight6/TFTMuZeroAgent')
+                                     epilog='For more information, go to https://github.com/silverlight6/TFTMuZeroAgent')
     parser.add_argument('--starting_episode', '-se', dest='starting_episode', type=int, default=0,
                         help='Episode number to start the training. Used for loading checkpoints, disables loading if = 0')
     args = parser.parse_args()
 
     interface = AI_interface.AIInterface()
-    interface.train_model(starting_train_step = args.starting_episode)
+    interface.test_train_model()
+    #interface.train_model(starting_train_step = args.starting_episode)
     # interface.collect_dummy_data()
     # interface.testEnv()
     # interface.PPO_algorithm()
