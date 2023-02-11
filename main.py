a46e9a6217d127623d5e2df937d05e6d4207a75d
oliverjdlopez <97719688+oliverjdlopez@users.noreply.github.com>
2023-02-11 17:19:56 -0500

    Pushing simulator to silver

--- main.py
+++ main.py
@@ -18,9 +18,9 @@ def main():
     args = parser.parse_args()
 
     interface = AI_interface.AIInterface()
-    interface.train_model(starting_train_step=args.starting_episode)
+    #interface.train_model(starting_train_step=args.starting_episode)
     # interface.collect_dummy_data()
-    # interface.testEnv()
+    interface.testEnv()
     # interface.PPO_algorithm()
 
     #test_interface = TestInterface.AIInterface()
