b618237e55429becc3ac2ac96a575b6817a19771
Tomas <jtlorente@ekumenlabs.com>
2023-08-16 13:28:34 -0300

    v2 Checkpoint
    Signed-off-by: Tomas <jtlorente@ekumenlabs.com>

--- Simulator/utils.py
+++ Simulator/utils.py
@@ -8,25 +8,20 @@ from time import time
 def champ_binary_encode(n):
     return list(np.unpackbits(np.array([n], np.uint8))[2:8])
 
-
 def champ_binary_decode(array):
     temp = list(array.copy().astype(int))
     temp.insert(0, 0)
     temp.insert(0, 0)
     return np.packbits(temp, axis=-1)[0]
 
-
 def item_binary_encode(n):
     return list(np.unpackbits(np.array([n], np.uint8))[2:8])
-
 
 def champ_one_hot_encode(n):
     return np.eye(config.MAX_CHAMPION_IN_SET)[n]
 
-
 def item_one_hot_encode(n):
     return np.eye(9)[n]
-
 
 def one_hot_encode_number(number, depth):
     return np.eye(depth)[number]
