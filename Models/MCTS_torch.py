Added some unit tests for masking and MCTS Utils. Fixed some masking bugs in the MCTS, removed some of the string sampling. Sped up the MCTS tree

Models/MCTS_torch.py
@@ -24,9 +24,10 @@ class MCTS:
         self.NUM_ALIVE = config.NUM_PLAYERS
         self.num_actions = 0
         self.ckpt_time = time.time_ns()
-        self.default_string_mapping = util.create_default_mapping()
+        self.default_string_mapping, self.default_mask = util.create_default_mapping()
         self.max_depth_search = 0
         self.model_config = model_config
+        self.default_mask = torch.tensor(self.default_mask).to(config.DEVICE)
 
     def policy(self, observation):
         with torch.no_grad():
@@ -37,28 +38,26 @@ class MCTS:
 
             reward_pool = np.array(network_output["reward"]).reshape(-1).tolist()
 
-            policy_logits = network_output["policy_logits"].cpu().numpy()
+            policy_logits = network_output["policy_logits"]
+            # Mask illegal actions
 
-            # 0.005 seconds
-            policy_logits_pool, string_mapping = self.encode_action_to_str(policy_logits, observation[1])
+            # is it quicker to do this as a tensor or as a numpy array?
+            flat_mask = torch.tensor(np.reshape(observation[1], (self.NUM_ALIVE, -1))).to(config.DEVICE)
+            inf_mask = torch.clamp(torch.log(flat_mask), min=-3.4e38)
+            policy_logits = policy_logits + inf_mask
+            masked_policy_logits = policy_logits.cpu().numpy()
 
             noises = [
-                    np.random.dirichlet([self.model_config.ROOT_DIRICHLET_ALPHA] * len(policy_logits_pool[j])
+                    np.random.dirichlet([self.model_config.ROOT_DIRICHLET_ALPHA] * len(masked_policy_logits[j])
                                         ).astype(np.float32).tolist()
                     for j in range(self.NUM_ALIVE)
                 ]
 
-            # policy_logits_pool = policy_logits_pool.cpu().numpy()
-            # print(policy_logits_pool)
-            # print(policy_logits_pool.shape)
-
-            # Policy Logits -> [ [], [], [], [], [], [], [], [],]
-
-            policy_logits_pool = self.add_exploration_noise(policy_logits_pool, noises)
+            policy_logits_pool = self.add_exploration_noise(masked_policy_logits, noises)
 
             # 0.001 seconds
             policy_logits_pool, string_mapping, mappings, policy_sizes = \
-                self.sample(policy_logits_pool, string_mapping, self.model_config.NUM_SAMPLES)
+                self.sample(policy_logits_pool, self.default_string_mapping, self.model_config.NUM_SAMPLES)
 
             # less than 0.0001 seconds
             # Setup specialised roots datastructures, format: env_nums, action_space_size, num_simulations
@@ -88,7 +87,7 @@ class MCTS:
                 action = self.select_action(distributions, temperature=temp, deterministic=deterministic)
                 actions.append(string_mapping[i][action])
                 target_policy.append([x / self.model_config.NUM_SIMULATIONS for x in distributions])
-
+                index_in_default = self.default_string_mapping[0].index(string_mapping[i][action])
             # Notes on possibilities for other dimensions at the bottom
             self.num_actions += 1
             return actions, target_policy, string_mapping, root_values
@@ -135,11 +134,17 @@ class MCTS:
             if diff > 150.:
                 print(f"EUREKA, VALUES MAX: {max(value_pool)}, AND MIN: {min(value_pool)}, RANGE {diff}")
 
-            policy_logits = network_output["policy_logits"].cpu().numpy()
+            policy_logits = network_output["policy_logits"]
+            # Mask illegal actions
+
+            default_mask = self.default_mask.repeat(self.NUM_ALIVE, 1)
+            inf_mask = torch.clamp(torch.log(default_mask), min=-3.4e38)
+            policy_logits = policy_logits + inf_mask
+            masked_policy_logits = policy_logits.cpu().numpy()
 
             # 0.003 to 0.01 seconds
             policy_logits, _, mappings, policy_sizes = \
-                self.sample(policy_logits, self.default_string_mapping, self.model_config.NUM_SAMPLES)
+                self.sample(masked_policy_logits, self.default_string_mapping, self.model_config.NUM_SAMPLES)
 
             # These assignments take 0.0001 > time
             # add nodes to the pool after each search
@@ -209,8 +214,8 @@ class MCTS:
     """
     def encode_action_to_str(self, policy_logits, mask):
         batch_size = policy_logits.shape[0]  # 8
-        flat_mask = np.reshape(mask, (self.NUM_ALIVE, -1))
-        policy_logits = policy_logits * flat_mask
+        # flat_mask = np.reshape(mask, (self.NUM_ALIVE, -1))
+        # policy_logits = policy_logits * flat_mask
 
         masked_policy_mappings = []
         masked_policy_logits = []
@@ -280,7 +285,6 @@ class MCTS:
                       Number of samples per player, can change if legal actions < num_samples
     """
     def sample(self, policy_logits, string_mapping, num_samples):
-        # policy_logits [(8, 7), (8, 5), (8, 667), (8, 370), (8, 38)]
         batch_size = len(policy_logits)  # 8
 
         output_logits = []

Models/MuZero_torch_agent.py
@@ -4,7 +4,7 @@ import torch
 import Models.MCTS_Util as utils
 import config
 from Models.abstract_model import AbstractNetwork
-from Models.torch_layers import mlp, MultiMlp, Normalize, MemoryLayer
+from Models.torch_layers import mlp, MultiMlp, Normalize, MemoryLayer, AlternateFeatureEncoder
 
 
 class MuZeroNetwork(AbstractNetwork):
@@ -146,7 +146,8 @@ class PredNetwork(torch.nn.Module):
         super().__init__()
 
         def feature_encoder(input_size, layer_sizes, output_size):
-            return torch.nn.Sequential(mlp(input_size, layer_sizes, output_size)).to(config.DEVICE)
+            return torch.nn.Sequential(AlternateFeatureEncoder(input_size, layer_sizes,
+                                                               output_size, config.DEVICE)).to(config.DEVICE)
 
         # 6 is the number of separate dynamic networks
         self.prediction_value_network = feature_encoder(model_config.HIDDEN_STATE_SIZE,
@@ -192,7 +193,7 @@ class RepNetwork(torch.nn.Module):
 
         def feature_encoder(input_size, feature_layer_sizes, feature_output_sizes):
             return torch.nn.Sequential(
-                mlp(input_size, feature_layer_sizes, feature_output_sizes, torch.nn.ReLU),
+                AlternateFeatureEncoder(input_size, feature_layer_sizes, feature_output_sizes, config.DEVICE),
                 Normalize()
             ).to(config.DEVICE)
 
@@ -235,13 +236,13 @@ class DynNetwork(torch.nn.Module):
                 Normalize()
             ).to(config.DEVICE)
 
-        self.action_encodings = mlp(config.ACTION_CONCAT_SIZE, [
-            model_config.LAYER_HIDDEN_SIZE] * 0, model_config.HIDDEN_STATE_SIZE)
+        self.action_encodings = AlternateFeatureEncoder(config.ACTION_CONCAT_SIZE, [
+            model_config.LAYER_HIDDEN_SIZE] * 0, model_config.HIDDEN_STATE_SIZE, config.DEVICE)
 
         self.dynamics_memory = memory()
 
-        self.dynamics_reward_network = mlp(input_size, [model_config.LAYER_HIDDEN_SIZE] * 1,
-                                           model_config.ENCODER_NUM_STEPS)
+        self.dynamics_reward_network = AlternateFeatureEncoder(input_size, [model_config.LAYER_HIDDEN_SIZE] * 1,
+                                           model_config.ENCODER_NUM_STEPS, config.DEVICE)
         self.model_config = model_config
 
     def forward(self, hidden_state, action):

Simulator/battle_generator.py
@@ -4,11 +4,33 @@ import random
 from Simulator.pool import pool
 from Simulator.player import Player
 from Simulator.utils import coord_to_x_y, x_y_to_1d_coord
-from Simulator.item_stats import item_builds
+from Simulator.item_stats import item_builds, thieves_gloves_items
 from Simulator.champion import champion
 from Simulator.observation.token.action import ActionToken
 from Simulator.default_agent_stats import ONE_COST_UNITS, TWO_COST_UNITS, THREE_COST_UNITS, FOUR_COST_UNITS, FIVE_COST_UNITS
 
+
+base_level_config = {
+    "num_unique_champions": 3,
+    "max_cost": 1,
+    "num_items": 0,
+    "current_level": 3,
+    "chosen": False,
+    "sample_from_pool": False,
+    "two_star_unit_percentage": 0,
+    "three_star_unit_percentage": 0,
+    "scenario_info": True,
+    "extra_randomness": False,
+    "stationary": True,
+    "azir": False,
+    "kayn": False,
+    "thieves_gloves": False,
+    "set_test_position": False,
+    "test_position": [0, 0],
+    "fill_bench": False,
+    "test_mode": False,
+    "allow_trait_items": False,
+}
 
 """
 Description - This is the Battle Generator class
@@ -32,6 +54,11 @@ class BattleGenerator:
         if self.generator_config["stationary"]:
             random.seed(8)
             self.stationary_coords = random.sample(list(range(0, 28)), 12)
+            if self.generator_config["set_test_position"]:
+                if x_y_to_1d_coord(self.generator_config["test_position"][0],
+                                   self.generator_config["test_position"][1]) not in self.stationary_coords:
+                    self.stationary_coords[0] = x_y_to_1d_coord(self.generator_config["test_position"][0],
+                                                                self.generator_config["test_position"][1])
 
 
     """
@@ -60,15 +87,21 @@ class BattleGenerator:
             player.shop = base_pool.sample(player, 5, allow_chosen=allow_chosen)
             player.shop_champions = player.create_shop_champions()
             if self.generator_config["scenario_info"]:
-                player.gold = np.random.randint(0, 60)
+                if self.generator_config["test_mode"]:
+                    player.gold = np.random.randint(20, 60)
+                    player.round = np.random.randint(3, 30)
+                else:
+                    player.gold = np.random.randint(0, 60)
+                    player.round = np.random.randint(0, 100) % (level * 3)
                 player.exp = np.random.randint(0, player.level_costs[level])
                 player.health = np.random.randint(1, 101)
-                player.round = np.random.randint(0, 100) % (level * 3)
             action_mask = ActionToken(player)
             self.add_champions(player, action_mask, base_pool)
             # Add these back in later after I see proof of learning
             self.add_items_to_champions(player, action_mask, item_count)
             self.add_items_to_item_bench(player)
+            if self.generator_config["fill_bench"]:
+                self.add_champions_to_bench(player, base_pool)
         player_returns = random.sample(range(0, 8), 2)
         return [player_list[player_returns[0]], player_list[player_returns[1]],
                 {f"player_{player.player_num}": player for player in player_list}]
@@ -81,7 +114,13 @@ class BattleGenerator:
         for i in range(player.max_units):
             if self.sample_from_pool:
                 random_champ = base_pool.sample(player, 1, allow_chosen=False)
-                success = player.add_to_bench(champion(random_champ[0]))
+                if i == 0 and (self.generator_config["azir"] or self.generator_config["kayn"]):
+                    if self.generator_config["azir"]:
+                        success = player.add_to_bench(champion('azir'))
+                    else:
+                        success = player.add_to_bench(champion('kayn'))
+                else:
+                    success = player.add_to_bench(champion(random_champ[0]))
                 if not success:
                     print("I was not successful")
                     continue
@@ -128,22 +167,44 @@ class BattleGenerator:
             for y in range(len(player.board[0])):
                 if player.board[x][y]:
                     for i in range(item_count):
-                        random_item = random.sample(list(item_builds.keys()), 1)
-                        player.add_to_item_bench(random_item[0])
-                        item_mask = action_mask.create_item_action_mask(player)
-                        if item_mask[move_failures][x_y_to_1d_coord(x, y)] and \
-                                not (random_item[0] == "thieves_gloves" and i > 0):
-                            player.move_item(move_failures, x, y)
+                        if i == 0 and self.generator_config["thieves_gloves"]:
+                            player.add_to_item_bench("thieves_gloves")
+                            item_mask = action_mask.create_item_action_mask(player)
+                            if item_mask[move_failures][x_y_to_1d_coord(x, y)]:
+                                player.move_item(move_failures, x, y)
+                            else:
+                                move_failures += 1
                         else:
-                            move_failures += 1
-                            if move_failures > 9:
-                                break
+                            if self.generator_config["allow_trait_items"]:
+                                random_item = random.sample(list(item_builds.keys()), 1)
+                            else:
+                                random_item = random.sample(thieves_gloves_items, 1)
+                            player.add_to_item_bench(random_item[0])
+                            item_mask = action_mask.create_item_action_mask(player)
+                            if item_mask[move_failures][x_y_to_1d_coord(x, y)] and \
+                                    not (random_item[0] == "thieves_gloves" and i > 0):
+                                player.move_item(move_failures, x, y)
+                            else:
+                                move_failures += 1
+                                if move_failures > 9:
+                                    break
 
     def add_items_to_item_bench(self, player):
-        for _ in range(8):
-            if not player.item_bench_full(1):
+        while not player.item_bench_full(1):
+            if self.generator_config["allow_trait_items"]:
                 random_item = random.sample(list(item_builds.keys()), 1)
-                player.add_to_item_bench(random_item[0])
+            else:
+                random_item = random.sample(thieves_gloves_items, 1)
+            player.add_to_item_bench(random_item[0])
+
+    def add_champions_to_bench(self, player, base_pool):
+        i = 0
+        while not player.bench_full():
+            random_champ = base_pool.sample(player, 1, allow_chosen=False)
+            player.add_to_bench(champion(random_champ[0]))
+            i += 1
+            if i > 10:
+                print(f"ADD_CHAMPIONS_TO_BENCH has an issue {i}")
 
 def sample_with_limit(units, x, seed=None):
     """Samples x units from the list, but returns the whole list if x is larger.