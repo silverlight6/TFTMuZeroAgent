Replaced the representation network with a token based one, fixed some masking errors, replaced the encode_to_str in mcts_torch entirely with a vector implementation, cleaned up bugs related to the refactor, expanded on the test_set to the MCTS_Utils and expanded the battle_generator to allow for more test_cases

Models/MCTS_torch.py
@@ -31,10 +31,10 @@ class MCTS:
 
     def policy(self, observation):
         with torch.no_grad():
-            self.NUM_ALIVE = observation[0]["shop"].shape[0]
+            self.NUM_ALIVE = observation["observations"]["shop"].shape[0]
 
             # 0.013 seconds
-            network_output = self.network.initial_inference(observation[0])
+            network_output = self.network.initial_inference(observation["observations"])
 
             reward_pool = np.array(network_output["reward"]).reshape(-1).tolist()
 
@@ -42,7 +42,7 @@ class MCTS:
             # Mask illegal actions
 
             # is it quicker to do this as a tensor or as a numpy array?
-            flat_mask = torch.tensor(np.reshape(observation[1], (self.NUM_ALIVE, -1))).to(config.DEVICE)
+            flat_mask = torch.tensor(np.reshape(observation["action_mask"], (self.NUM_ALIVE, -1))).to(config.DEVICE)
             inf_mask = torch.clamp(torch.log(flat_mask), min=-3.4e38)
             policy_logits = policy_logits + inf_mask
             masked_policy_logits = policy_logits.cpu().numpy()
@@ -87,7 +87,6 @@ class MCTS:
                 action = self.select_action(distributions, temperature=temp, deterministic=deterministic)
                 actions.append(string_mapping[i][action])
                 target_policy.append([x / self.model_config.NUM_SIMULATIONS for x in distributions])
-                index_in_default = self.default_string_mapping[0].index(string_mapping[i][action])
             # Notes on possibilities for other dimensions at the bottom
             self.num_actions += 1
             return actions, target_policy, string_mapping, root_values
@@ -193,72 +192,6 @@ class MCTS:
 
         return action_pos
 
-    """
-    Description - Turns a 1081 action into a policy that includes only actions that are legal in the current state
-                  This also creates a mask for both the c++ side and python side to convert the legal action set into
-                  a single action that we can give to the buffers and the trainer.
-                  Masks for this method are generated in the player and observation classes.
-                  This is only called by the root node since that is the only node that has access to the observation
-    Inputs      - Policy logits: List
-                      output of the prediction network, initial_inference in this case
-                  Mappings: List
-                      A mask of binary values that tell the policy what actions are legal and what actions are not.
-    Outputs     - Actions: List
-                      A policy including actions that are legal in the field.
-                  Mappings: List
-                      A byte mapping that maps those actions to a single 3 dimensional action that can be used in the 
-                      simulator as well as in the recurrent inference. This gets sent to the c++ side
-                  Seconds Mappings: List
-                      A string mapping that is used in the same way but for the python side. This gets used on the 
-                      values that get sent back to the AI_Interface
-    """
-    def encode_action_to_str(self, policy_logits, mask):
-        batch_size = policy_logits.shape[0]  # 8
-        # flat_mask = np.reshape(mask, (self.NUM_ALIVE, -1))
-        # policy_logits = policy_logits * flat_mask
-
-        masked_policy_mappings = []
-        masked_policy_logits = []
-
-        for batch in range(batch_size):
-            masked_dim_mapping = []
-            masked_pol_logits = []
-            for dim in range(len(mask[batch])):
-                if dim < 37:
-                    for idx in range(len(mask[batch][dim])):
-                        if mask[batch][dim][idx]:
-                            if idx < 37:
-                                masked_dim_mapping.append(f"5_{dim}_{idx}")
-                                masked_pol_logits.append(policy_logits[batch][dim * 38 + idx])
-                            else:
-                                masked_dim_mapping.append(f"4_{dim}")
-                                masked_pol_logits.append(policy_logits[batch][dim * 38 + 37])
-                elif dim < 47:
-                    for idx in range(len(mask[batch][dim])):
-                        if mask[batch][dim][idx]:
-                            masked_dim_mapping.append(f"6_{dim - 37}_{idx}")
-                            masked_pol_logits.append(policy_logits[batch][dim * 38 + idx])
-                elif dim < 52:
-                    for idx in range(5):
-                        if mask[batch][dim][idx]:
-                            masked_dim_mapping.append(f"3_{dim - 47}")
-                            masked_pol_logits.append(policy_logits[batch][dim * 38])
-                elif dim == 52:
-                    if mask[batch][dim][0]:
-                        masked_dim_mapping.append(f"0")
-                        masked_pol_logits.append(policy_logits[batch][dim * 38])
-                elif dim == 53:
-                    if mask[batch][dim][0]:
-                        masked_dim_mapping.append(f"1")
-                        masked_pol_logits.append(policy_logits[batch][dim * 38])
-                elif dim == 54:
-                    if mask[batch][dim][0]:
-                        masked_dim_mapping.append(f"2")
-                        masked_pol_logits.append(policy_logits[batch][dim * 38])
-            masked_policy_mappings.append(masked_dim_mapping)
-            masked_policy_logits.append(masked_pol_logits)
-
-        return masked_policy_logits, masked_policy_mappings
 
     """
     Description - This is the core to the Complex Action Spaces paper. We take a set number of sample actions from the 

Models/PositionModels/action_mask_model.py
@@ -103,15 +103,16 @@ class TorchPositionEncoderModel(torch.nn.Module):
         full_embeddings = torch.cat([champion_emb, champion_item_emb, champion_trait_emb], dim=-1)
         full_embeddings = torch.reshape(full_embeddings, (cie_shape[0], cie_shape[1] * cie_shape[2], -1))
 
-        seq_length = 224  # Number of tokens in the sequence (224 in your case)
-
         # Create a position index [0, 1, 2, ..., 223] repeated for each sample in the batch
-        positions = torch.arange(0, seq_length).unsqueeze(0).expand(cie_shape[0], seq_length).to(config.DEVICE)
+        positions = torch.arange(full_embeddings.shape[1], dtype=torch.long, device=config.DEVICE).unsqueeze(0)
+        print(positions.shape)
 
         # Get the positional encodings and add them to the full embeddings
         positional_enc = self.positional_embedding(positions)
+        print(positional_enc.shape)
         # print(f"positional_enc {positional_enc}")
         full_embeddings = full_embeddings + positional_enc
+        print(full_embeddings.shape)
 
         # Expand the classification token to match the batch size
         cls_tokens = self.cls_token.expand(cie_shape[0], -1, -1)

Models/torch_layers.py
@@ -196,7 +196,7 @@ class TransformerEncoder(nn.Module):
         super(TransformerEncoder, self).__init__()
 
         self.encoder_layers = nn.ModuleList([
-            nn.TransformerEncoderLayer(d_model, n_heads, d_hidden, dropout, batch_first=True)
+            nn.TransformerEncoderLayer(d_model, n_heads, d_hidden, dropout, batch_first=True).to(config.DEVICE)
             for _ in range(n_layers)
         ])
 

Simulator/observation/token/basic_observation.py
@@ -43,18 +43,15 @@ class ObservationToken(ObservationBase, ObservationUpdateBase):
         self.game_scalars = self.create_game_scalars(player)
 
         # --- Public Scalars --- #
-        self.public_scalars = self.create_public_scalars(
-            player)
+        self.public_scalars = self.create_public_scalars(player)
 
         # --- Private Scalars --- #
-        self.private_scalars = self.create_private_scalars(
-            player)
+        self.private_scalars = self.create_private_scalars(player)
 
         # --- Public Vectors --- #
         self.board_vector = self.create_board_vector(player)
         self.bench_vector = self.create_bench_vector(player)
-        self.item_bench_vector = self.create_item_bench_vector(
-            player)
+        self.item_bench_vector = self.create_item_bench_vector(player)
         self.trait_vector = self.create_trait_vector(player)
 
         # --- Private Vectors --- #
@@ -292,6 +289,7 @@ class ObservationToken(ObservationBase, ObservationUpdateBase):
             # TODO: Action History
         ])
 
+    # TODO: Turn health and gold into embeddable numbers.
     def create_public_scalars(self, player):
         """Create public scalars for a player
 
@@ -376,15 +374,18 @@ class ObservationToken(ObservationBase, ObservationUpdateBase):
 
         # -2 because 1 and 2 are not used if not there.
         champion_vector[0] = list(COST.keys()).index(champion.name) * 3 + champion.stars - 2
+        assert champion_vector[0] < 221
 
         for i, item_id in enumerate(champion.items):
             if item_id is not None:
                 champion_vector[i + 1] = list(items.keys()).index(item_id)
+                assert champion_vector[i + 1] < 58
 
         is_chosen = True if champion.chosen else False
 
-        trait_number = list(origin_class.keys()).index(champion.name) * 2
-        champion_vector[-1] = trait_number * 2 if is_chosen else trait_number
+        trait_number = list(origin_class.keys()).index(champion.name)
+        champion_vector[-1] = trait_number + len(origin_class.keys()) if is_chosen else trait_number
+        assert champion_vector[-1] < 145
 
         return champion_vector
 
@@ -405,12 +406,12 @@ class ObservationToken(ObservationBase, ObservationUpdateBase):
                         Right
 
         Rotated to match the board in game
-                                Top
-        | (0, 3) (1, 3) (2, 3) (3, 3) (4, 3) (5, 3) (6, 3) |
-  Left  | (0, 2) (1, 2) (2, 2) (3, 2) (4, 2) (5, 2) (6, 2) |
-        | (0, 1) (1, 1) (2, 1) (3, 1) (4, 1) (5, 1) (6, 1) |  Right
-        | (0, 0) (1, 0) (2, 0) (3, 0) (4, 0) (5, 0) (6, 0) |
-                                Bottom
+                                    Top
+            | (0, 3) (1, 3) (2, 3) (3, 3) (4, 3) (5, 3) (6, 3) |
+      Left  | (0, 2) (1, 2) (2, 2) (3, 2) (4, 2) (5, 2) (6, 2) |
+            | (0, 1) (1, 1) (2, 1) (3, 1) (4, 1) (5, 1) (6, 1) |  Right
+            | (0, 0) (1, 0) (2, 0) (3, 0) (4, 0) (5, 0) (6, 0) |
+                                    Bottom
 
         """
 
@@ -436,7 +437,7 @@ class ObservationToken(ObservationBase, ObservationUpdateBase):
             | (0) (1) (2) (3) (4) (5) (6) (7) (8) |
 
         """
-        bench_vector = np.zeros((9, self.champion_vector_length), dtype=np.float32)
+        bench_vector = np.zeros((9, self.champion_vector_length), dtype=np.int16)
 
         for idx, champion in enumerate(player.bench):
             if champion:
@@ -475,11 +476,12 @@ class ObservationToken(ObservationBase, ObservationUpdateBase):
             | (0) (1) (2) (3) (4) (5) (6) (7) (8) (9) |
 
         """
-        item_bench_vector = np.zeros(10, dtype=np.float32)
+        item_bench_vector = np.zeros(10, dtype=np.int16)
 
         for i, item in enumerate(player.item_bench):
             if item:
                 item_bench_vector[i] = self.util.get_item_id(item)
+                assert(item_bench_vector[i]) < 58
 
         return item_bench_vector
 
@@ -510,19 +512,17 @@ class ObservationToken(ObservationBase, ObservationUpdateBase):
         return safe_normalize(tiers_vector)
 
     @staticmethod
-    def observation_to_input(observation):
+    def observation_to_position_input(observation):
         other_players = {
             "board":
                 np.array(
                     [observation["opponents"][x]["board"] for x in range(config.NUM_PLAYERS - 1)], dtype=np.int16
                 )
-
             ,
             "traits":
                 np.array(
                     [observation["opponents"][x]["traits"] for x in range(config.NUM_PLAYERS - 1)],
                 )
-
             ,
         }
         return {
@@ -530,6 +530,35 @@ class ObservationToken(ObservationBase, ObservationUpdateBase):
                                      other_players["board"]], axis=0).astype(np.int16),
             "traits": np.concatenate([np.expand_dims(observation["player"]["traits"], axis=0),
                                       other_players["traits"]], axis=0, dtype=np.float32),
+        }
+
+    def observation_to_input(self, observation):
+        other_players = {
+            "board":
+                np.array(
+                    [observation["opponents"][x]["board"] for x in range(config.NUM_PLAYERS - 1)], dtype=np.int16
+                )
+            ,
+            "traits":
+                np.array(
+                    [observation["opponents"][x]["traits"] for x in range(config.NUM_PLAYERS - 1)],
+                )
+            ,
+            "scalars":
+                np.reshape(np.array(
+                    [observation["opponents"][x]["scalars"] for x in range(config.NUM_PLAYERS - 1)]
+                ), (1, -1)),
+        }
+        return {
+            "board": np.concatenate([np.expand_dims(observation["player"]["board"], axis=0),
+                                     other_players["board"]], axis=0).astype(np.int16),
+            "traits": np.concatenate([np.expand_dims(observation["player"]["traits"], axis=0),
+                                      other_players["traits"]], axis=0, dtype=np.float32),
+            "bench": np.expand_dims(observation["player"]["bench"], axis=0),
+            "items": np.expand_dims(observation["player"]["items"], axis=0),
+            "shop": observation["player"]["shop"],
+            "scalars": np.concatenate([np.expand_dims(observation["player"]["scalars"], axis=0),
+                                      other_players["scalars"]], axis=1, dtype=np.float32),
         }
 
     @staticmethod